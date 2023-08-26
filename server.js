7c8f12bfa15c01cf2fb174d9fd4552a1ab11e53c
adriennemadarang <adriennemadarang@icloud.com>
2023-08-25 21:16:42 -0400

    added routes folder

--- server.js
+++ server.js
@@ -3,7 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// add routes
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+// add routes
+
+app.listen(PORT, () => {
+    console.log(`API server now listening on port ${PORT}`);
+});
