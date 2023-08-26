7a5c746126f5e811ed49a8d75c004198240fabf3
adriennemadarang <adriennemadarang@icloud.com>
2023-08-25 22:53:46 -0400

    fixed server and updated folders

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 
+// GET route for notes
 router.get('/notes', (req, res) => {
 
 });
-
+// POST route to update notes
 router.post('/notes', (req, res) => {
 
 });
 
+// DELETE route to delete note
 router.delete('/notes', (req, res) => {
 
 });
--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// add routes
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static('public'));
 
-// add routes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now listening on port ${PORT}`);
