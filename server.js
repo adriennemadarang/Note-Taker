75ae9a1ad2a6984b75857e3cfd1f8c549292f021
adriennemadarang <adriennemadarang@icloud.com>
2023-08-29 19:04:53 -0700

    updated serverjs

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const fs = require ('fs');
 const PORT = process.env.PORT || 3001;
 
-const htmlRoutes = require('./routes/htmlRoutes');
-const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
+app.use(html_routes);
+app.use(api_routes);
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now listening on port ${PORT}`);
+    console.log(`API server now listening at http://localhost:${PORT} `)
 });
