f52b112139e8b1cef494542816ddc90a2186b618
adriennemadarang <adriennemadarang@icloud.com>
2023-08-29 21:34:14 -0700

    added utils js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
-const uuid = require('../helpers/uuid');
+// const uuid = require('../helpers/uuid');
 const fs = require('fs');
 
+// GET request
 router.get('/api/notes', async(req, res) => {
-    const note = await JSON.parse(fs.readFileSync('db/db.json'))
+    const notes = await JSON.parse(fs.readFileSync('db/db.json'))
     res.json(note);
 });
 
+// POST request
+router.post('/api/notes', (req,res) => {
+    const notes = JSON.parse(fs.readFileSync('db/db.json'))
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid()
+    };
+    notes.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(notes));
+    res.json(notes);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const app = express();
 const fs = require ('fs');
 const PORT = process.env.PORT || 3001;
+const htmlRoutes = require ('./routes/htmlRoutes');
+const apiRoutes = require ('./routes/apiRoutes');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(htmlRoutes);
-app.use(apiRoutes);
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 
 app.listen(PORT, () => {
