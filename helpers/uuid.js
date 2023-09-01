52284a3ce5f53efc4163fc4e4e2f487323a38bc3
adriennemadarang <adriennemadarang@icloud.com>
2023-08-31 20:19:35 -0700

    fixed server

--- helpers/uuid.js
+++
@@ -1,4 +0,0 @@
-module.exports = () =>
-    Math.floor((1 + Math.randon() * 0x10000))
-        .toString(16)
-        .substring(1);
--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const fs = require ('fs');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + 'public'));
+app.use(express.static('public'));
 
-require('./routes/apiRoutes')(app);
-
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now listening at http://localhost:${PORT} `)
